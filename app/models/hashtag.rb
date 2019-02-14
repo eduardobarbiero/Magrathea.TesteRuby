3450a51bf229f3d9110a0cacb1dad1071e811176
Eduardo Barbiero <eduardombarbiero@gmail.com>
2019-02-14 03:21:32 -0200

    Decidido passar a validação de formato de dado para o modelo.

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,5 +1,5 @@
 class Hashtag < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true, format: { with: /[a-zA-Z0-9]/ }
 
-    has_many :messages
+    has_many :messages, dependent: :destroy
 end
